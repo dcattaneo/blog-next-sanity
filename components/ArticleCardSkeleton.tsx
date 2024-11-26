cde1c53b0824a97130d58e0e1f951827b9be32a3
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-11-25 22:35:35 -0300

    articleCardSkeleton styles

--- components/ArticleCardSkeleton.tsx
+++ components/ArticleCardSkeleton.tsx
@@ -5,7 +5,7 @@ export const ArticleCardSkeleton = () => {
     <>
       {[0, 1, 2, 3].map((index: number) => (
         <li key={index}>
-          <Skeleton className="w-full h-96 rounded-[22px] bg-gray-400" />
+          <Skeleton className="w-full h-96 rounded-[22px] bg-[#00000014] dark:bg-[#111] dark:border dark:border-solid dark:border-[#333]" />
         </li>
       ))}
     </>
