9050e8b848e3ecede1c65fcace9c3f7eb651c8c7
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-11-28 14:35:20 -0300

    fix 404 not found styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 const NotFound = () => {
   return (
     <div className="not_found_container">
-      <h1 className="heading  text-4xl font-bold text-gradient dark:text-gradient-dark">
+      <h1 className="heading text-4xl font-bold text-gradient">
         404 - Page Not Found
       </h1>
       <p className="sub-heading font-normal mt-4">
         Sorry, the page you are looking for does not exist.
       </p>
       <Link href="/" className="mt-6 px-6 py-2  ">
-        <Button className="bg-custom-light  shadow-none hover:bg-[#00000014] dark:bg-[#0a0a0a] dark:text-white dark:hover:bg-[#ffffff17] dark:bg-[#4e4c4c14] dark:border-[#ffffff24] ">
+        <Button className="bg-custom-light  shadow-none hover:bg-[#00000014]">
           Go Back Home
         </Button>
       </Link>
